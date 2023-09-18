b61d186a742676b2893c865fb580d37e9a4989a1
Imat Marasigan <raymart.marasigan@gmail.com>
2023-09-18 19:32:45 +0800

    Add deposit button when user is bidding

--- pages/components/ui/Modal.tsx
+++ pages/components/ui/Modal.tsx
@@ -25,7 +25,7 @@ const Modal: React.FC<ModalProps> = ({ isOpen, children, onClose }) => {
 	if (!isOpen || !el) return null;
 
 	return ReactDOM.createPortal(
-		<div className="fixed inset-0 flex items-center justify-center z-40">
+		<div className="fixed inset-0 flex items-center justify-center z-[999] ">
 			<div className="bg-white p-10 rounded w-full md:w-1/2 shadow-lg z-50">
 				{children}
 			</div>
